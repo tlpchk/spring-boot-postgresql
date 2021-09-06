d5a2ccdfcdb6f71fb18c0f3040219e9cb794eed5
tlpchk <max.telepchuk@gmail.com>
2021-09-06 15:07:00 +0200

    init project

---
+++ src/main/java/com/project/postgredemo/PostgreDemoApplication.java
@@ -0,0 +1,13 @@
+package com.project.postgredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgreDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgreDemoApplication.class, args);
+	}
+
+}
